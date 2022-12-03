036731efff59a332e6df309e8b6ebe5becf1dd63
Alex Ly <aly26@csu.fullerton.edu>
2022-12-03 12:33:31 -0800

    fixed js

--- main.js
+++ main.js
@@ -1,41 +1,124 @@
-let board = document.getElementsByClassName('tile');
+const player = "O";
+const computer = "X";
 
-let ways_to_win = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+let complete_board = false;
+let board = ["", "", "", "", "", "", "", "", ""];
 
-let player1 = 'X';
-let player2 = 'O';
-let turn = 0;
-let total_rows = 8;
+const board_container = document.querySelector(".game_area");
 
-for (let i = 0; i < board.length; i++) {
-    board[i].addEventListener('click', (e) => {
-        if (turn == 0) {
-            board[i].textContent = player1;
-            check_winner()
-            turn = 1;
-        }
-        else {
-            board[i].textContent = player2;
-            check_winner()
-            turn = 0;
-        }
-    })
-}
-function check_winner() {
-    for (let i = 0; i < total_rows; i++) {
-        if ( (board[ways_to_win[i][0]].textContent != '') &&
-             (board[ways_to_win[i][0]].textContent == board[ways_to_win[i][1]].textContent) &&
-             (board[ways_to_win[i][1]].textContent == board[ways_to_win[i][2]].textContent) ) {
-                console.log(board[ways_to_win[i][0]].textContent + ' wins');
-             }
+const winner_statement = document.getElementById("winner");
+
+check_complete_board = () => {
+  let flag = true;
+  board.forEach(element => {
+    if (element != player && element != computer) {
+      flag = false;
     }
+  });
+  complete_board = flag;
 };
+
+
+const check_line = (a, b, c) => {
+  return (
+    board[a] == board[b] &&
+    board[b] == board[c] &&
+    (board[a] == player || board[a] == computer)
+  );
+};
+
+const check_match = () => {
+  for (i = 0; i < 9; i += 3) {
+    if (check_line(i, i + 1, i + 2)) {
+      document.querySelector(`#block_${i}`).classList.add("win");
+      document.querySelector(`#block_${i + 1}`).classList.add("win");
+      document.querySelector(`#block_${i + 2}`).classList.add("win");
+      return board[i];
+    }
+  }
+  for (i = 0; i < 3; i++) {
+    if (check_line(i, i + 3, i + 6)) {
+      document.querySelector(`#block_${i}`).classList.add("win");
+      document.querySelector(`#block_${i + 3}`).classList.add("win");
+      document.querySelector(`#block_${i + 6}`).classList.add("win");
+      return board[i];
+    }
+  }
+  if (check_line(0, 4, 8)) {
+    document.querySelector("#block_0").classList.add("win");
+    document.querySelector("#block_4").classList.add("win");
+    document.querySelector("#block_8").classList.add("win");
+    return board[0];
+  }
+  if (check_line(2, 4, 6)) {
+    document.querySelector("#block_2").classList.add("win");
+    document.querySelector("#block_4").classList.add("win");
+    document.querySelector("#block_6").classList.add("win");
+    return board[2];
+  }
+  return "";
+};
+
+const check_for_winner = () => {
+  let res = check_match()
+  if (res == player) {
+    winner.innerText = "Player Wins!";
+    winner.classList.add("playerWin");
+    complete_board = true
+  } else if (res == computer) {
+    winner.innerText = "Computer Wins!";
+    winner.classList.add("computerWin");
+    complete_board = true
+  } else if (complete_board) {
+    winner.innerText = "Draw!";
+    winner.classList.add("draw");
+  }
+};
+
+
+const render_board = () => {
+  board_container.innerHTML = ""
+  board.forEach((e, i) => {
+    board_container.innerHTML += `<div id="block_${i}" class="block" onclick="addPlayerMove(${i})">${board[i]}</div>`
+    if (e == player || e == computer) {
+      document.querySelector(`#block_${i}`).classList.add("occupied");
+    }
+  });
+};
+
+const game_loop = () => {
+  render_board();
+  check_complete_board();
+  check_for_winner();
+}
+
+const addPlayerMove = e => {
+  if (!complete_board && board[e] == "") {
+    board[e] = player;
+    game_loop();
+    addComputerMove();
+  }
+};
+
+const addComputerMove = () => {
+  if (!complete_board) {
+    do {
+      selected = Math.floor(Math.random() * 9);
+    } while (board[selected] != "");
+    board[selected] = computer;
+    game_loop();
+  }
+};
+
+const reset_board = () => {
+  board = ["", "", "", "", "", "", "", "", ""];
+  complete_board = false;
+  winner.classList.remove("playerWin");
+  winner.classList.remove("computerWin");
+  winner.classList.remove("draw");
+  winner.innerText = "";
+  render_board();
+};
+
+//initial render
+render_board();
