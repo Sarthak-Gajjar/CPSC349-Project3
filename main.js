a579256a104a0d15f2e1ad64b698eff0b323045a
Sarthak-Gajjar <sarthakgajjar@csu.fullerton.edu>
2022-12-03 16:53:07 -0800

    Final update!

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ const check_for_winner = () => {
     winner.classList.add("computerWin");
     complete_board = true
   } else if (complete_board) {
-    winner.innerText = "Draw!";
+    winner.innerText = "Tie!";
     winner.classList.add("draw");
   }
 };
